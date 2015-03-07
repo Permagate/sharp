7c0a78ca7920a84d34ed476da0fcdb98044dc3b7
Permagate <Kend654@gmail.com>
2015-03-07 17:33:42 +0700

    Cleanup and add index page test.

--- app/controllers/web_controller.rb
+++ app/controllers/web_controller.rb
@@ -2,18 +2,4 @@ class WebController < ApplicationController
   def index
     render file: Rails.root.join('web', 'app-view.html')
   end
-
-  def fetch
-  	*initial_urls, last_url = params[:templateurl].split('/')
-  	web_root = Rails.root.join('web')
-  	initial_render_path = initial_urls.reduce(web_root) { |current_path, url| current_path.join(url) }
-
-  	possible_render_paths = [
-  		initial_render_path.join("#{last_url}.html.erb"),
-  		initial_render_path.join("#{last_url}", "#{last_url}.html.erb")
-  	]
-  	render_path = possible_render_paths.find { |path| File.exists? path }
-  	
-    render file: render_path
-  end
 end
